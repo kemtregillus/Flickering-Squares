86f6aaf3b211a714cec278171345246489b5d6a0
kemtregillus <kemtregillus@gmail.com>
2016-06-22 16:36:41 +0800

    staircase improvements
    added while loop that will terminate at so many reversals. Case 1 will
    adjust the test patch freely, case 2 will only adjust 1 upscale or
    downscale at a time. Not sure if that’s ok.

--- flickerSquaresStair.m
+++ flickerSquaresStair.m
@@ -1,7 +1,7 @@
 %%%%%%%%Code written by Katie Tregillus Spring & summer 2016
 %%%%%%%%This code is designed to test adaptation to a mean flicker rate
 %%%%%%%%Contact Katie with questions: kmussell@gmail.com
-
+ 
 %% Clear the workspace and the screen
 sca;
 close all;
@@ -12,8 +12,8 @@ screens = Screen('Screens');
 Screen('Preference', 'SkipSyncTests', 1);
 %% set up keys
 KbName('UnifyKeyNames');
-upKey = KbName('UpArrow');
-downKey = KbName('DownArrow');
+rightKey = KbName('RightArrow');
+leftKey = KbName('LeftArrow');
 enterKey = KbName('RETURN');
 escapeKey = KbName('ESCAPE');
 %% set mean and sd for stim squares
@@ -21,9 +21,6 @@ escapeKey = KbName('ESCAPE');
 % mean = input(prompt);
 % prompt   2 = 'What is the sd value? ';
 % sd = i nput(prompt2);
-mean = 5;
-sd = 3;
-mean2 = 5;
 
 screenNumber = max(screens);
 % screenNumber = 0 ;
@@ -35,36 +32,39 @@ black = BlackIndex(screenNumber);
 baseRect = [0 0 65 65]; %%stim squares size
 %rRect = [0 0 100 100]; %%response squares size
 fixation = [0 0 20 20];  %%fixation size
-
 [xCenter, yCenter] = RectCenter(windowRect);%%center points
 %%%distance away from fixation with these setting is 125 pixels
 xFSquares = xCenter-500;  %%location stim squares - overall width of square matix is 375
 yFSquares = yCenter-225;
 xRSquare = xCenter+50;  %%location response squares
 yRSquare = yCenter-225;
-
-%% matrix of probabalistically distributed frequencies
-freqMat = abs(normrnd(mean,sd,5,5)); %%frequency of stim squares
-respFreq = abs(normrnd(mean2,sd,5,5));  %%response square freq
-respFreqnew = respFreq;
-phaseMat = abs(rand(5,5).*10);
-
-
 rectColor = [1 1 1];
 pStart = GetSecs;
 experiment = 3;
-ntrials = 10;
-adapTime = 2;
-topUp = 5;
+nTrials = 2;
+adapTime = 10;
+topUp = 5; 
 testTime = 2;
 curTrial = 0;
 upScale = 1.2;
 downScale = 0.8;
 
-keyResp = zeros(ntrials,1);
+keyResp = zeros(50,1);
 stairCount = 0;
 %%do it
-for k = 1:ntrials
+mean = 5;
+sd = 3;
+mean2 = 5;
+%% matrix of probabalistically distributed frequencies
+freqMat = abs(normrnd(mean,sd,5,5)); %%frequency of stim squares
+respFreq = abs(normrnd(mean2,sd,5,5));  %%response square freq
+respFreqnew = respFreq;
+phaseMat = abs(rand(5,5).*10);
+reversals = 5;
+
+for k = 1:nTrials
+    while stairCount <= reversals
+    
     switch  experiment
         
         case 1 %%%Adaptation Top-Up
@@ -87,12 +87,12 @@ for k = 1:ntrials
                 KbCheck;
                 [keyIsDown, seconds, keyCode ]  = KbCheck;
                 if keyIsDown
-                    if keyCode(upKey)
-                        respFreqnew = respFreq.*upScale;
+                    if keyCode(leftKey)
+                        respFreq = respFreq.*upScale;
                         keyResp(curTrial,1) = 'U';
                         
-                    elseif keyCode(downKey)
-                        respFreqnew = respFreq.*downScale;
+                    elseif keyCode(rightKey)
+                        respFreq = respFreq.*downScale;
                         keyResp(curTrial,1) = 'D';
                         
                     end
@@ -105,7 +105,7 @@ for k = 1:ntrials
                 Screen('FillOval',window, [.75 .75 .75],fixRect);
                 Screen('Flip', window);
             end
-            respFreqnew = respFreq;
+%             respFreqnew = respFreq;
             
             experiment = 2;
             
@@ -130,11 +130,11 @@ for k = 1:ntrials
                 KbCheck;
                 [keyIsDown, seconds, keyCode ]  = KbCheck;
                 if keyIsDown
-                    if keyCode(upKey)
+                    if keyCode(leftKey)
                         respFreqnew = respFreq.*upScale;
                         keyResp(curTrial,1) = 'U';
                         
-                    elseif keyCode(downKey)
+                    elseif keyCode(rightKey)
                         respFreqnew = respFreq.*downScale;
                         keyResp(curTrial,1) = 'D';
                         
@@ -163,7 +163,7 @@ for k = 1:ntrials
                 Screen('FillOval',window, [.75 .75 .75],fixRect);
                 Screen('Flip', window);
             end
-            respFreq = respFreqnew;
+            respFreq = respFreqnew
             %%increase staircase counter
             if curTrial > 1 && keyResp(curTrial,1) == 'U' && keyResp(curTrial-1,1) == 'D'
                 stairCount = stairCount+1
@@ -199,6 +199,13 @@ for k = 1:ntrials
             end
             experiment = 2;
     end
+    end
+    experiment = 3;
+    stairCount = 0;
+    mean = 10;
+    Screen('FillOval',window, [.75 .75 .75],fixRect);
+    Screen('Flip', window);
+    KbStrokeWait;
 end
 
 KbStrokeWait;
