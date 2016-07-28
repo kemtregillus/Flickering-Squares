24dc98ea8c64b8f7c8ab72cba040e5d15497d92a
kemtregillus <kemtregillus@gmail.com>
2016-07-28 17:21:16 +0800

    ensemble
    analysis code for ensemble experiment

---
+++ analysis_ensemble.m
@@ -0,0 +1,30 @@
+clear all
+close all
+
+% load 'KT2_ensemble_22-Jul-2016'
+load 'wk_ensemble_28-Jul-2016.mat'
+
+
+p1 = scatter(outAdapt.means(2,:),outAdapt.responses(2:26)');
+hold on
+p = polyfit(outAdapt.means(2,:),outAdapt.responses(2:26)',1);
+f = polyval(p,outAdapt.means(2,:));
+plot(outAdapt.means(2,:),f,'--b');
+plot(outAdapt.means(1,:),outAdapt.means(1,:),'-k')
+
+
+% load 'kt2_ensemble_28-Jul-2016.mat'
+load 'wk3_ensemble_28-Jul-2016.mat'
+
+p2 = scatter(outAdapt.means(3,:),outAdapt.responses(2:26)');
+hold on
+p = polyfit(outAdapt.means(3,:),outAdapt.responses(2:26)',1);
+f = polyval(p,outAdapt.means(3,:));
+plot(outAdapt.means(3,:),f,'--r');
+axis('equal')
+axis([0 6 0 6])
+xlabel('Ensemble Mean (Hz)')
+ylabel('Response (Hz)')
+legend([p1,p2],'Ensemble Estimate','Single Square','Location','NorthWest')
+
+
